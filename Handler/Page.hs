32b3ab71b73e371a7c5d9c57b634e129bc522986
unknown <>
2017-05-09 11:53:34 +0300

    first commit

---
+++ Handler/Page.hs
@@ -0,0 +1,20 @@
+module Handler.Page where
+
+import Import
+import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
+import Text.Julius (RawJS (..))
+
+-- This is a handler function for the GET request method on the HomeR
+-- resource pattern. All of your resource patterns are defined in
+-- config/routes
+--
+-- The majority of the code you will write in Yesod lives in these handler
+-- functions. You can spread them across multiple files if you are so
+-- inclined, or create a single monolithic file.
+getPageR :: Handler Html
+getPageR = do
+    let handlerName = "getHomeR" :: Text    
+    defaultLayout $ do
+        aDomId <- newIdent
+        setTitle "Page 1"
+        $(widgetFile "storiespage")
