8465b1c61e87a5d435da1781b90e6a374dcb32fa
unknown <>
2017-06-23 17:24:47 +0300

    getting html

--- Handler/Page.hs
+++ Handler/Page.hs
@@ -3,10 +3,12 @@ module Handler.Page where
 import Import
 import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
 import Text.Julius (RawJS (..))
---import Network.HTTP.Simple
---import Data.Text
---import Data.List
+import Network.HTTP
+
+import Data.Text (pack, concat, unpack)
+import Data.List (foldl)
 import qualified Data.ByteString.Char8 as L8
+--import Prelude
 
 -- This is a handler function for the GET request method on the HomeR
 -- resource pattern. All of your resource patterns are defined in
@@ -16,8 +18,8 @@ import qualified Data.ByteString.Char8 as L8
 -- functions. You can spread them across multiple files if you are so
 -- inclined, or create a single monolithic file.
 
---doStuffWithNormalString :: String -> Text
---doStuffWithNormalString s = Data.Text.pack " kkhkhkjj"
+doStuffWithNormalString :: IO String
+doStuffWithNormalString = simpleHTTP (Network.HTTP.getRequest "http://www.zerohedge.com/?page=1") >>= fmap (take 10000) . getResponseBody
 
 
 --getzhpage :: IO String
@@ -32,20 +34,34 @@ getPageR = do
     --datas <- liftIO getzhpage
     --let stories = (doStuffWithNormalString datas) :: Text
 
-    --req' <- liftIO $ parseUrl "http://localhost:3000/"
+    --manager <- liftIO $ newManager defaultManagerSettings
+    --req' <- liftIO $ parseRequest "http://www.zerohedge.com/?page=1"
     --let req = req' { method = "POST", requestBody = RequestBodyLBS valueBS }
     --let stories = httpLBS "http://www.zerohedge.com/?page=1"
 
     $logDebug "Trying to read data file"
-    edata <- liftIO $ try $ readFile "E:/DEV/Haskell/testyesod/README.md"
-    case edata :: Either IOException ByteString of
+    --edata <- liftIO $ try $ readFile "E:/DEV/Haskell/testyesod/README.md"
+    --request <- parseRequest "http://httpbin.org/post"
+
+    --Network.HTTP.Conduit.withManager $ \manager -> do
+    --req' <- liftIO $ parseUrl "http://www.zerohedge.com/?page=1"
+    --let req = req' { method = "GET" }
+    --res <- http req  manager
+    --resValue <- responseBody res
+--theResponse <- liftIO $ try $ responseBody . httpLbs "http://www.zerohedge.com/?page=1"
+
+    --let edata = liftIO $ try $ doStuffWithNormalString
+    edata <- liftIO $ try $ doStuffWithNormalString
+    -- let edata = map (\x -> Data.Text.pack x) edata1
+    -- edata <- liftIO $ try $ readFile "E:/DEV/Haskell/testyesod/README.md"
+    case edata :: Either IOException String of
         Left e -> do
             $logError $ "Could not read datafile.txt"
             defaultLayout [whamlet|An error occurred|]
         Right str -> do
             $logInfo "Reading of data file succeeded"
-            let ls = Import.lines (L8.unpack str)
-            when (Import.length ls < 5) $ $logWarn "Less than 5 lines of data"
+            let ls = Import.lines (Data.Text.pack str)
+            when (Import.length ls < 50) $ $logWarn "Less than 5 lines of data"
             defaultLayout
                 [whamlet|
                     <ol>
@@ -53,6 +69,14 @@ getPageR = do
                             <li>#{l}
                 |]
 
+    --let ls = Import.lines (Data.Text.unpack (Data.Text.concat edata))
+    --when (Import.length ls < 5) $ $logWarn "Less than 5 lines of data"
+    -- defaultLayout
+    --     [whamlet|
+    --         <ol>
+    --             $forall l <- edata1
+    --                 <li>#{l}
+    --     |]
     -- let handlerName = "getPageR" :: Text    
 
     -- defaultLayout $ do
